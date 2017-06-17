c710aabe2b117a007831259827c86842d18c6e1b
Tianxiao Zhao <tzh@kth.se>
2017-06-17 23:14:19 +0200

    Add files via upload

---
+++ php/filtered-ls.php
@@ -0,0 +1,10 @@
+<?php
+	/* Create a program that prints a list of files in a given directory, filtered by the extension of the files. You will be provided a directory name as the first argument to your program (e.g. '/path/to/dir/') and a file extension to filter by as the second argument. */
+
+	foreach (new DirectoryIterator($argv[1]) as $file) {
+		if ($file->getExtension() == $argv[2]){
+			echo $file->getFilename() . "\n";
+		}
+    }
+
+?>
