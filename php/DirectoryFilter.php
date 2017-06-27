e7204ac3b84049e364fce03dceb6556dce3757cf
Tianxiao Zhao <tzh@kth.se>
2017-06-27 22:39:14 +0200

    Update DirectoryFilter.php

--- php/DirectoryFilter.php
+++ php/DirectoryFilter.php
@@ -1,5 +1,5 @@
 <?php
-	/*class file: contains directory reading and extension filtering. */
+	/* class file: contains directory reading and extension filtering. */
 	
     	class DirectoryFilter
     	{
