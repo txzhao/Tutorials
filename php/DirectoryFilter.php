5d2fa9e774ab8fde4dabaa9d3af4809f9f4206d9
Tianxiao Zhao <tzh@kth.se>
2017-06-27 22:38:46 +0200

    Update DirectoryFilter.php

--- php/DirectoryFilter.php
+++ php/DirectoryFilter.php
@@ -1,17 +1,17 @@
 <?php
+	/*class file: contains directory reading and extension filtering. */
 	
-	
-    class DirectoryFilter
-    {
-        public function filter($args)
-        {
-        	$filtered_f = "";
-            foreach (new DirectoryIterator($args[1]) as $file) {
+    	class DirectoryFilter
+    	{
+        	public function filter($args)
+        	{
+        		$filtered_f = "";
+            		foreach (new DirectoryIterator($args[1]) as $file) {
 				if ($file->getExtension() == $args[2]){
 					$filtered_f .= $file->getFilename() . "\n";
 				}
 			}
 			return $filtered_f;
-        }
-    }
+        	}
+    	}
     
