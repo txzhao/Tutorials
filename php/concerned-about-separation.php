c6e894d32799ee126eef9e1a70341fe3e649d920
Tianxiao Zhao <tzh@kth.se>
2017-06-27 22:34:39 +0200

    Update concerned-about-separation.php

--- php/concerned-about-separation.php
+++ php/concerned-about-separation.php
@@ -1,4 +1,8 @@
 <?php
+	/*Create a program that prints a list of files in a given directory, filtered by the extension of the files. 
+	The first argument is the directory name and the second argument is the extension filter. 
+	Print the list of files (one file per line) to the console.*/
+
 	require_once __DIR__ . '/DirectoryFilter.php';
 	$myFilter = new DirectoryFilter();
 	echo $myFilter->filter($argv);
