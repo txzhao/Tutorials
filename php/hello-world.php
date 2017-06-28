eed3e6b37208272e2dd34367c4d915dd7e7d9713
Tianxiao Zhao <tzh@kth.se>
2017-06-28 23:50:44 +0200

    Update hello-world.php

--- php/hello-world.php
+++ php/hello-world.php
@@ -1,3 +1,4 @@
 <?php
-	/* Write a program that prints the text "Hello World" to the console (stdout). */
-	echo "Hello World";
+/* Write a program that prints the text "Hello World" to the console (stdout). */
+
+echo "Hello World";
