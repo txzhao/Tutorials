86d26d6bec5031d0260012520997f4f7a8b904ce
Tianxiao Zhao <tzh@kth.se>
2017-06-28 23:49:41 +0200

    Update baby-steps.php

--- php/baby-steps.php
+++ php/baby-steps.php
@@ -1,17 +1,18 @@
 <?php
-	/* Write a program that accepts one or more numbers as command-line arguments 
-	and prints the sum of those numbers to the console (stdout).*/
+/* Write a program that accepts one or more numbers as command-line arguments 
+and prints the sum of those numbers to the console (stdout). */
 
-	// display structure information
-	//var_dump($argv);
+// display structure information
+//var_dump($argv);
 	
-	$sum = 0;
-	$iter = 0;
-	foreach ($argv as &$value) {
-		if ($iter != 0) {
-			$sum += $value;
-		}
-		$iter ++;
+$sum = 0;
+$iter = 0;
+foreach ($argv as &$value) {
+	if ($iter != 0) {
+		$sum += $value;
 	}
-	echo $sum;
+	$iter ++;
+}
+echo $sum;
+
 ?>
