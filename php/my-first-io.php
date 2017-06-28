a0c21d85c9c76db3f662a2159321aa0e859396bc
Tianxiao Zhao <tzh@kth.se>
2017-06-28 23:51:05 +0200

    Update my-first-io.php

--- php/my-first-io.php
+++ php/my-first-io.php
@@ -1,8 +1,8 @@
 <?php
-	/* Write a program that uses a single filesystem operation to read a file 
-	and print the number of newlines (\n) it contains to the console (stdout). */
+/* Write a program that uses a single filesystem operation to read a file 
+and print the number of newlines (\n) it contains to the console (stdout). */
 
-	$f_contents = file_get_contents($argv[1]);	// read a file into a string
-	echo substr_count($f_contents, "\n");		// count the number of substring occurances
+$f_contents = file_get_contents($argv[1]);	// read a file into a string
+echo substr_count($f_contents, "\n");		// count the number of substring occurances
 
 ?>
