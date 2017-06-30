7e3f930b9b538a27643720b3a9111003bc4b7f6f
Tianxiao Zhao <tzh@kth.se>
2017-06-30 22:43:25 +0200

    Update exceptional-coding.php

--- php/exceptional-coding.php
+++ php/exceptional-coding.php
@@ -1,8 +1,7 @@
 <?php
 /* Write a program that takes an array of filepaths as arguments and outputs the basename of each, seperated by a new line.
 Every file should exist but under exceptional circumstances some files may not. If this occurs, output a message similar to the below.
-Unable to open file at path '/file/path'
-*/
+Unable to open file at path '/file/path' */
 
 array_shift($argv);
 foreach ($argv as $f_path) {
